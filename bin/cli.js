d5ecfa54827677092ba42f7a849a0d65e47de8cb
sundowndev <raphael@crvx.fr>
2019-04-01 18:11:05 +0200

    Build files

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,70 +12,87 @@ const underbase_1 = require("underbase");
 const yargs = require("yargs");
 const path = require("path");
 const fs = require("fs");
+require = require("esm")(module);
 ;
-;
+const logger = (level, ...arg) => console.log(`[${level}]`, ...arg);
 const argv = yargs
+    .scriptName("underbase-cli")
     .usage('Usage: $0 <command> [OPTIONS]')
     .command('migrate <migration>', 'Execute migrations')
     .command('list', 'Show all migrations versions')
     .command('status', 'Show migrations status')
-    .describe('db', 'MongoDB connection URL')
-    .describe('migrations-dir', 'Migrations versions directory')
+    .describe('db <url>', 'MongoDB connection URL')
+    .describe('migrations-dir <dir>', 'Migrations versions directory')
     .describe('backups', 'Enable automatic backups')
-    .describe('backups-dir', 'Backups directory')
-    .describe('collection-name', 'Migrations state collection')
+    .describe('backups-dir <dir>', 'Backups directory')
+    .describe('collection-name <name>', 'Migrations state collection')
     .describe('logs', 'Enable logs')
-    .help('h')
+    .describe('force', 'Force migrations execution')
+    .describe('chdir <dir>', 'Change the working directory')
+    .describe('version', 'Show underbase-cli package version')
+    .help('h', 'Show this help message')
     .alias('h', 'help')
     .locale('en_US')
-    .argv;
-let metro = {};
-try {
-    metro = require(argv.config);
+    .parse();
+let configFile = {};
+const workingDirectory = argv.chdir || process.cwd();
+if (argv.config) {
+    configFile = require(path.join(workingDirectory, argv.config));
 }
-catch (err) { }
 const config = {
-    log: argv.logs || metro.logs || true,
-    logger: (level, ...arg) => console.log(`[${level}]`, ...arg),
+    log: argv.logs || configFile.logs || true,
+    logger: logger,
     logIfLatest: true,
-    collectionName: argv.collectionName || metro.collectionName || 'migrations',
-    db: argv.db || metro.db || "mongodb://localhost:27017/api",
-    backups: argv.backups || metro.backups || false,
-    backupsDir: path.resolve(argv.backupsDir || metro.backupsDir || './migrations/backups'),
-    migrationsDir: path.resolve(argv.migrationsDir || metro.migrationsDir || './migrations'),
+    collectionName: argv.collectionName || configFile.collectionName || 'migrations',
+    db: argv.db || configFile.db || null,
+    backups: argv.backups || configFile.backups || false,
+    backupsDir: path.resolve(argv.backupsDir || configFile.backupsDir || './migrations/backups'),
+    migrationsDir: path.resolve(argv.migrationsDir || configFile.migrationsDir || './migrations'),
 };
 (() => __awaiter(this, void 0, void 0, function* () {
-    yield underbase_1.migrator.config(config);
+    if (!argv._[0]) {
+        yargs.help();
+        process.exit();
+    }
+    if (!fs.existsSync(config.migrationsDir)) {
+        fs.mkdirSync(config.migrationsDir);
+        config.logger('info', 'Created migration directory.');
+    }
     let versions = fs.readdirSync(config.migrationsDir)
         .filter((v) => v.match(new RegExp(/^[\d].[\d]$/)));
     switch (argv._[0]) {
         case 'migrate':
-            if (!fs.existsSync(config.migrationsDir)) {
-                config.logger('info', 'Created migration directory.');
-                fs.mkdirSync(config.migrationsDir);
-            }
             versions = versions.map((v) => parseFloat(v));
             if (argv.migration != 0 && versions.indexOf(parseFloat(argv.migration)) < 0) {
-                console.log('This version does not exists.');
+                logger('error', 'This version does not exists.');
                 process.exit();
             }
             versions = versions.map((v) => v.toFixed(1));
+            yield underbase_1.migrator.config(config);
             versions.forEach((v) => __awaiter(this, void 0, void 0, function* () {
-                const migrationObj = require(`${config.migrationsDir}/${v}`).default;
+                const migrationObj = yield require(`${config.migrationsDir}/${v}`).default;
                 yield underbase_1.migrator.add(migrationObj);
             }));
-            yield underbase_1.migrator.migrateTo(argv.migration);
+            if (config.backups) {
+                logger('info', 'create backup');
+            }
+            if (argv.force)
+                yield underbase_1.migrator.migrateTo(`${argv.migration},rerun`);
+            else
+                yield underbase_1.migrator.migrateTo(argv.migration);
             break;
         case 'list':
+            logger('info', 'Versions list based on folders');
             versions.forEach((v) => console.log(v));
             break;
         case 'status':
+            yield underbase_1.migrator.config(config);
+            const currentVersion = yield underbase_1.migrator.getVersion();
+            logger('info', `Current version is ${currentVersion}`);
             break;
         default:
-            console.log('show help');
+            console.error('You should be doing', yargs.help());
             break;
     }
-    if (config.backups) { }
     process.exit();
 }))();
-//# sourceMappingURL=cli.js.map
